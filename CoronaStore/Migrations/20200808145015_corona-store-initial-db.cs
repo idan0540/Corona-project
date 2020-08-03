334426b4d2ef8865a8c1a52b7c0be91ad50a3977
idan h <idan0540@gmail.com>
2020-08-03 16:26:50 +0300

    fixed tweets config and finished views connections

---
+++ CoronaStore/Migrations/20200808145015_corona-store-initial-db.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoronaStore.Migrations
+{
+    public partial class coronastoreinitialdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Population",
+                table: "Products",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "Inventory",
+                table: "Products",
+                newName: "SupplierID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Population",
+                table: "Locations",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "Locations",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Locations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Locations",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Locations",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.CreateTable(
+                name: "Inventory",
+                columns: table => new
+                {
+                    InventoryID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductID = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventory", x => x.InventoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    SaleID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductID = table.Column<int>(nullable: false),
+                    UserID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.SaleID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    SupplierID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.SupplierID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventory");
+
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Locations");
+
+            migrationBuilder.RenameColumn(
+                name: "SupplierID",
+                table: "Products",
+                newName: "Inventory");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Products",
+                newName: "Population");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Population",
+                table: "Locations",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Locations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
---
+++ CoronaStore/Services/LocationsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using CoronaStore.Models;
+
+namespace CoronaStore.Services
+{
+    public class LocationsService
+    {
+        public IList<Location> SearchLocations(string name, int? population)
+        {
+            using (var context = new CoronaPageContext())
+            {
+                IQueryable<Location> locations = context.Locations.Where(p => p.City.ToLower().Contains(name.ToLower()));
+
+                if (population != null)
+                {
+                    locations = locations.Where(p => p.Population <= population);
+                }
+
+                return locations.ToList();
+            }
+        }
+
+
+    }
+}
